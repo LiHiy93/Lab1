489073a98b492febaa8340bf8166c1a3763bdfa0
LiHiy93 <150333101+LiHiy93@users.noreply.github.com>
2024-10-05 00:52:25 +0300

    Create tests.h

---
+++ lab1/lab1/tests.h
@@ -0,0 +1,18 @@
+//
+// Created by Alexey Kossov on 30.03.2024.
+//
+
+#ifndef LABORATORY1_TESTS_H
+#define LABORATORY1_TESTS_H
+
+
+
+void test_init_vector();
+void test_push_vector();
+void test_sort_vector();
+void test_concatenation();
+void test_where();
+void test_map();
+void test_str_functions();
+
+#endif //LABORATORY1_TESTS_H
